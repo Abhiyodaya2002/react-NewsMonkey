c68a2cf3214e0a5f69253406cdd4fea3cb5c1147
abhiyodaya <abhiyodayapandey@gmail.com>
2022-08-09 20:46:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  
+} from "react-router-dom";
+/* 
+Before using react-router-dom the class based component looked like this.
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+  render() {
+    return (
+      
+      <div>
+         <Navbar/>
+         <News pagesize={6} category={"science"}/>
+      </div>
+    )
+  }
 }
+export default App;
+*/
 
+class App extends Component {
+  render() {
+    return (
+      <>
+    
+      <div>
+         <Navbar/>
+      <Switch>
+      <Route exact path="/"><News key="home" pagesize={6} category="general"/></Route>
+      <Route exact path="/business"><News key="business" pagesize={6} category="business"/></Route>
+      <Route exact path="/entertainment"><News key="entertainment" pagesize={6} category="entertainment"/></Route>
+      
+      <Route exact path="/health"><News key="health" pagesize={6} category="health"/></Route>
+      <Route exact path="/science"><News key="science" pagesize={6} category="science"/></Route>
+      <Route exact path="/sports"><News key="sports" pagesize={6} category="sports"/></Route>
+      <Route exact path="/technology"><News key="technology" pagesize={6} category="technology"/></Route>
+    </Switch>
+    </div>
+    
+    </>
+    )
+  }
+}
 export default App;
